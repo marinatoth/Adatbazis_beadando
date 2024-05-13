b0a58fc766ddf1267b46c73546c75f7cd1116df7
vrekaa121 <153096114+vrekaa121@users.noreply.github.com>
2024-05-13 22:41:58 +0200

    Update lekerdezesek.sql

--- lekerdezesek.sql
+++ lekerdezesek.sql
@@ -1,6 +1,24 @@
+-- 1. lekérdezés
+
 select t.nev
 from oktatok ok join tanulok t on ok.id = t.oktato_id 
 	join orarend ora on ora.tanulo_id= t.id 
     join kategoriak k on k.kat_id=ok.kategoria_id 
     join napok n on n.id=ora.nap_id
 where n.nap = 'Hétfő' and k.rovidites = 'B'
+
+-- 2. lekérdezés
+	
+SELECT IIF(GROUPING_ID(k.rovidites) = 1, 'Végösszeg', CAST(k.rovidites as nvarchar(4))) AS 'Jogosítvány típusa',
+       CASE 
+         WHEN GROUPING_ID(k.rovidites, o.nev) = 1 THEN 'Részösszeg'
+         WHEN GROUPING_ID(k.rovidites, o.nev) = 3 THEN 'Végösszeg'
+         ELSE o.nev 
+       END AS 'Oktató neve',
+       COUNT(*) AS 'Tanulók száma'
+FROM jarmuvek j JOIN oktatok o on j.jarmu_id = o.jarmu_id
+				JOIN kategoriak k ON j.kategoria_id = k.kat_id
+                JOIN tanulok t ON o.id = t.oktato_id
+GROUP BY ROLLUP(k.rovidites, o.nev)
+
+-- 3. lekérdezés
