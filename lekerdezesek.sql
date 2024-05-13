d427056d31ae123873128b13df42324da81ca961
marinatoth <152974077+marinatoth@users.noreply.github.com>
2024-05-14 00:32:42 +0200

    Update lekerdezesek.sql

--- lekerdezesek.sql
+++ lekerdezesek.sql
@@ -1,9 +1,10 @@
 -- 1. lekérdezés
 
 select t.nev as 'Tanuló neve'
-from oktatok ok join tanulok t on ok.id = t.oktato_id 
+from  jarmuvek j join oktatok ok on j.jarmu_id = ok.jarmu_id 
+	join tanulok t on ok.id = t.oktato_id 
 	join orarend ora on ora.tanulo_id= t.id 
-    join kategoriak k on k.kat_id=ok.kategoria_id 
+    join kategoriak k on k.kat_id=j.kategoria_id 
     join napok n on n.id=ora.nap_id
 where n.nap = 'Hétfő' and k.rovidites = 'B'
 
