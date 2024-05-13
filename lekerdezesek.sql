71219bfb4de28bf01da0184058574a25a8a7543a
Blankanator <134514898+Blankanator@users.noreply.github.com>
2024-05-13 21:47:48 +0200

    Update lekerdezesek.sql
    Marina
    kik azok a tanulok, akiknek hetfon van orajuk es B kategoriat tanulnak

--- lekerdezesek.sql
+++ lekerdezesek.sql
@@ -1,1 +1,6 @@
-
+select t.nev
+from oktatok ok join tanulok t on ok.id = t.oktato_id 
+	join orarend ora on ora.tanulo_id= t.id 
+    join kategoriak k on k.kat_id=ok.kategoria_id 
+    join napok n on n.id=ora.nap_id
+where n.nap = 'Hétfő' and k.rovidites = 'B'
